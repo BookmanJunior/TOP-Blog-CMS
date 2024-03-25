c546df9361b6dc9b249d38eb3f9e1da0fed47716
Azam Gulov <gulovazam@gmail.com>
2024-03-25 19:35:18 +0500

    Add link to articles

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,6 +4,7 @@ export default function Dashboard() {
   return (
     <div className="dashboard">
       <NavLink to={'users'}>Users</NavLink>
+      <NavLink to={'articles'}>Articles</NavLink>
     </div>
   );
 }
