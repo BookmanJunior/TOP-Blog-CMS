54f18a406880959c649165a8c75e369fbdaf8de9
Azam Gulov <gulovazam@gmail.com>
2024-03-25 19:35:47 +0500

    ADd users, articles, comments count

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,23 @@
+import FetchData from '../components/FetchData';
+
+type DocumentTypeCount = {
+  userCount: number;
+  articleCount: number;
+  commentCount: number;
+};
+
 export default function Home() {
-  return <h2>This is Home</h2>;
+  const { data, loading, error } = FetchData<DocumentTypeCount>('http://localhost:3000/cms/count');
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Something wen't wrong!</p>;
+
+  return (
+    <div>
+      <p>{`Users: ${data?.userCount}`}</p>
+      <p>{`Articles: ${data?.articleCount}`}</p>
+      <p>{`Comments: ${data?.commentCount}`}</p>
+    </div>
+  );
 }
