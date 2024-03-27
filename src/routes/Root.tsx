16fa65d8bd79df1a54abb126c960b47760ff92b9
Azam Gulov <gulovazam@gmail.com>
2024-03-27 14:11:05 +0500

    Add outlet context
    add outlet context type
    make dashboard rendering conditional

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,12 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
+import { useState } from 'react';
+
+type UserType = {
+  username: string;
+  role: string;
+};
+
+type RootContextType = {
+  user: UserType;
+  setUser: (arg0: UserType) => void;
+};
 
 export default function Root() {
+  const [user, setUser] = useState();
+
   return (
     <>
-      <Dashboard />
-      <h1>This is root</h1>
-      <Outlet />
+      {user && <Dashboard />}
+      <Outlet context={{ user, setUser }} />
     </>
   );
+}
+
+export function UseUser() {
+  return useOutletContext<RootContextType>();
 }
