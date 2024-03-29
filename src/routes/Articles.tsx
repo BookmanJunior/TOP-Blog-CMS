9af19de07ac58e7a9150a8844a1d7d9ed6d27ea5
Azam Gulov <gulovazam@gmail.com>
2024-03-29 16:03:30 +0500

    Add CheckBox component
    add PublishedCheckBoxComponent

--- src/routes/Articles.tsx
+++ src/routes/Articles.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 import { ArticleType } from '../Types/ArticleType';
 import FetchData from '../components/FetchData';
+import { useEffect, useRef, useState } from 'react';
+import { format } from 'date-fns';
+import DeleteButton from '../components/DeleteButton';
 
 export default function Articles() {
   const apiEndPoint = 'http://localhost:3000/articles';
-  const { data, error, loading } = FetchData<ArticleType[]>(apiEndPoint);
+  const { data, setData, error, loading } = FetchData<ArticleType[]>(apiEndPoint);
+  const [checkBoxStatus, setCheckBoxStatus] = useState(false);
 
   if (loading) return <span>Loading...</span>;
 
@@ -13,33 +17,146 @@ export default function Articles() {
   }
 
   return (
-    <table>
-      <caption>Articles</caption>
-      <thead>
-        <tr>
-          <th scope="col">title</th>
-          <th scope="col">author</th>
-          <th scope="col">ID</th>
-          <th scope="col">Date</th>
-          <th scope="col">Published</th>
-          <th scope="col">Featured</th>
-          <th scope="col">Comments</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data?.map((article) => (
-          <tr key={article._id}>
-            <th scope="row">{article.title}</th>
-            <td>{article.author.username}</td>
-            <td>{article._id}</td>
-            <td>{article.date}</td>
-            <td>{article.published + ''}</td>
-            <td>{article.featured + ''}</td>
-            <td>{article.comments.length}</td>
-            <td>{<Link to={`${article._id}`}>Edit</Link>}</td>
+    <>
+      <Link to={'create'}>Create article</Link>
+      <table>
+        <caption>Articles</caption>
+        <thead>
+          <tr>
+            <th scope="col">title</th>
+            <th scope="col">author</th>
+            <th scope="col">ID</th>
+            <th scope="col">Date</th>
+            <th scope="col">Comments</th>
+            <th scope="col">Published</th>
+            <th scope="col">Featured</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((article) => (
+            <tr key={article._id}>
+              <th scope="row">{article.title}</th>
+              <td>{article.author.username}</td>
+              <td>{article._id}</td>
+              <td>{format(article.date, 'PPP')}</td>
+              <td>{article.comments.length}</td>
+              <td>
+                <PublishedCheckBox article={article} setData={setData} />
+              </td>
+              <td>
+                <CheckBox
+                  article={article}
+                  setData={setData}
+                  name="featured"
+                  checkBoxStatus={checkBoxStatus}
+                  setCheckBoxStatus={setCheckBoxStatus}
+                />
+              </td>
+              <td>{<Link to={`${article._id}`}>Edit</Link>}</td>
+              <td>
+                <DeleteButton<ArticleType[]>
+                  title={article.title}
+                  setData={setData}
+                  apiEndPoint={`http://localhost:3000/cms/articles/${article._id}`}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
+}
+
+type PublishedCheckBoxProps = {
+  article: ArticleType;
+  setData: (prev: any) => void;
+};
+
+function PublishedCheckBox({ article, setData }: PublishedCheckBoxProps) {
+  const [publishedStatus, setPublishedStatus] = useState(false);
+
+  return (
+    <CheckBox
+      article={article}
+      setData={setData}
+      name="published"
+      checkBoxStatus={publishedStatus}
+      setCheckBoxStatus={setPublishedStatus}
+    />
+  );
+}
+
+type CheckBoxProps = {
+  name: 'published' | 'featured';
+  checkBoxStatus: boolean;
+  setCheckBoxStatus: (arg0: boolean) => void;
+} & PublishedCheckBoxProps;
+
+function CheckBox({ article, setData, name, checkBoxStatus, setCheckBoxStatus }: CheckBoxProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [flag, setFlag] = useState(false);
+
+  useEffect(() => {
+    if (flag) {
+      formRef.current?.requestSubmit();
+    }
+
+    return () => {
+      setFlag(false);
+    };
+  }, [flag]);
+
+  return (
+    <form onSubmit={handleSubmit} ref={formRef}>
+      <input
+        type="checkbox"
+        name={name}
+        checked={article[name]}
+        onChange={handleChange}
+        disabled={checkBoxStatus}
+      />
+    </form>
+  );
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setData((prev: ArticleType[]): ArticleType[] =>
+      prev.map((currentArticle) => {
+        if (currentArticle._id === article._id) {
+          const articleUpdate = { ...currentArticle, [name]: e.target.checked };
+          return articleUpdate;
+        }
+        return currentArticle;
+      })
+    );
+    setFlag(true);
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setCheckBoxStatus && setCheckBoxStatus(true);
+    try {
+      const res = await fetch(`http://localhost:3000/cms/articles/${article._id}/checkbox`, {
+        method: 'PUT',
+        mode: 'cors',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(article)
+      });
+
+      if (res.status >= 400) {
+        console.log('Error');
+        return;
+      }
+
+      const resResult: ArticleType[] = await res.json();
+      setData(resResult);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setCheckBoxStatus && setCheckBoxStatus(false);
+    }
+  }
 }
