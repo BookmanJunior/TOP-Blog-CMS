fa8a6a0222a177e50a269353ef4f657625bfb707
Azam Gulov <gulovazam@gmail.com>
2024-03-30 22:17:39 +0500

    Fix scss import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './routes/Router.tsx';
-import './styles/index.css';
+import '../src/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
