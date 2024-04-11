2313ae5213cea6e658b8ce7915aae3362cf82052
Azam Gulov <gulovazam@gmail.com>
2024-04-11 15:56:18 +0500

    Replace form components with ArticleForm component

--- src/routes/ArticleEditor.tsx
+++ src/routes/ArticleEditor.tsx
@@ -1,17 +1,13 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ArticleType } from '../Types/ArticleType';
 import FetchData from '../components/FetchData';
-import { ArticleContentEditor } from '../components/ArticleContentEditor';
-import { useRef } from 'react';
-import { MDXEditorMethods } from '@mdxeditor/editor';
+import ArticleForm from '../components/ArticleForm';
 
 export default function ArticleEditor() {
   const { articleId } = useParams();
-  const { data, setData, loading, error } = FetchData<ArticleType>(
+  const { data, loading, error } = FetchData<ArticleType>(
     `http://localhost:3000/articles/${articleId}`
   );
-  const mdxEditorRef = useRef<MDXEditorMethods>(null);
-  const navigate = useNavigate();
 
   if (loading) {
     return <p>Loading...</p>;
@@ -22,49 +18,10 @@ export default function ArticleEditor() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="article-form form-util">
-      <input
-        type="text"
-        name="cover"
-        id="cover"
-        value={data?.cover}
-        onChange={(e) => setData({ ...data, cover: e.target.value })}
-        placeholder="Cover..."
-      />
-      <input
-        type="text"
-        name="title"
-        id="title"
-        value={data?.title}
-        onChange={(e) => setData({ ...data, title: e.target.value })}
-        placeholder="Title..."
-      />
-      <ArticleContentEditor content={data.content} ref={mdxEditorRef} />
-      <button>Submit</button>
-    </form>
+    <ArticleForm
+      data={data}
+      fetchMethod="PUT"
+      apiEndPoint={`http://localhost:3000/cms/articles/${articleId}`}
+    />
   );
-
-  async function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const content = mdxEditorRef.current?.getMarkdown();
-
-    try {
-      const res = await fetch(`http://localhost:3000/cms/articles/${data._id}`, {
-        method: 'PUT',
-        mode: 'cors',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          ...data,
-          content: content
-        })
-      });
-
-      if (res.ok) {
-        navigate(`/articles`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
 }
--- src/routes/NewArticle.tsx
+++ src/routes/NewArticle.tsx
@@ -1,46 +1,6 @@
-import { useNavigate } from 'react-router-dom';
-import { ArticleContentEditor } from '../components/ArticleContentEditor';
-import { useRef } from 'react';
-import { MDXEditorMethods } from '@mdxeditor/editor';
+import ArticleForm from '../components/ArticleForm';
 import '../styles/ArticleEditor.scss';
 
 export default function NewArticle() {
-  const mdxEditorRef = useRef<MDXEditorMethods>(null);
-  const navigate = useNavigate();
-
-  return (
-    <form onSubmit={handleSubmit} className="article-form form-util">
-      <input type="text" name="cover" id="cover" placeholder="Cover" />
-      <input type="text" name="title" id="title" placeholder="Title" />
-      <ArticleContentEditor ref={mdxEditorRef} />
-      <button>Submit</button>
-    </form>
-  );
-
-  async function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const updatedData = Object.fromEntries(formData);
-    const content = mdxEditorRef.current?.getMarkdown();
-
-    try {
-      const res = await fetch(`http://localhost:3000/articles`, {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          cover: updatedData.cover,
-          title: updatedData.title,
-          content: content
-        })
-      });
-
-      if (res.ok) {
-        navigate(`/articles`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  return <ArticleForm fetchMethod="POST" apiEndPoint="http://localhost:3000/articles" />;
 }
