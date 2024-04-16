bf660bfeb763246a81de1b410556c8eea923c83f
Azam Gulov <gulovazam@gmail.com>
2024-04-16 16:37:42 +0500

    Remove NewArticle.tsx

--- src/routes/NewArticle.tsx
+++
@@ -1,6 +0,0 @@
-import ArticleForm from '../components/ArticleForm';
-import '../styles/ArticleEditor.scss';
-
-export default function NewArticle() {
-  return <ArticleForm fetchMethod="POST" apiEndPoint="http://localhost:3000/articles" />;
-}
