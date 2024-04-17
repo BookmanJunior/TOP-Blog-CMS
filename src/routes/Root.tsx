5a346befaa65fee4f7fe27b5e1d1c712ca8172a9
Azam Gulov <gulovazam@gmail.com>
2024-04-17 17:12:13 +0500

    Add useEffect to auto login if session cookie present
    return state if loading otherwise Outlet component

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useOutletContext } from 'react-router-dom';
 import Nav from '../components/Nav';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type UserType = {
   username: string;
@@ -14,17 +14,44 @@ type RootContextType = {
 
 export default function Root() {
   const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function autoLogin() {
+      try {
+        const res = await fetch('http://localhost:3000/admin/auto-login', {
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include'
+        });
+
+        if (res.ok) {
+          const resResult = await res.json();
+          setUser(resResult);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    autoLogin();
+  }, []);
 
   return (
     <>
       {user && <Nav />}
       <main>
-        <Outlet
-          context={{
-            user,
-            setUser
-          }}
-        />
+        {!loading ? (
+          <Outlet
+            context={{
+              user,
+              setUser
+            }}
+          />
+        ) : (
+          <div>Loading...</div>
+        )}
       </main>
     </>
   );
